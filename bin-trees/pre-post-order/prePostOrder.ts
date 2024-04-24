b96a8e9397e9945b9054dd41e6f93e0a95a928ce
Emily Walker <ewalker415@gmail.com>
2024-04-24 16:41:11 -0700

    binTrees

--- bin-trees/pre-post-order/prePostOrder.ts
+++ bin-trees/pre-post-order/prePostOrder.ts
@@ -4,15 +4,42 @@ import { BNodeNum } from "../common/bintree";
  * Returns an array of values of visited nodes. */
 
 function preOrder(node: BNodeNum | null): number[] {
-  return [42];
+  const result: number[] = [];
+
+  if (!node) return result;
+
+  result.push(node.val);
+
+  result.push(...preOrder(node.lnode), ...preOrder(node.rnode));
+
+  return result;
 }
+
+/**
+ *        6
+ *       / \
+ *      5  5
+ *
+ *
+ *
+ *
+ */
 
 
 /** dfsPostOrder(): Traverse from the invoking node using post-order DFS.
  * Returns an array of values of visited nodes. */
 
 function postOrder(node: BNodeNum | null): number[] {
-  return [42];
+  const result: number[] = [];
+
+  if (!node) return result;
+
+
+  result.push(...postOrder(node.lnode), ...postOrder(node.rnode));
+
+  result.push(node.val);
+
+  return result;
 }
 
 export { preOrder, postOrder };
